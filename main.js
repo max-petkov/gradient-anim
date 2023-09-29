823bdc626167f9e4fe4ff7487a75d8210a608e53
Maximilian Petkov <max@stan.vision>
2023-09-29 18:04:22 +0300

    Added Collision

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ function Circle(canvas, config) {
 	this.rMin = !config.rMin ? 100 : config.rMin;
 	this.minViewWidth = !config.minViewWidth ? 768 : config.minViewWidth;
 	this.maxViewWidth = !config.maxViewWidth ? 1920 : config.maxViewWidth;
+	this.mass = 1;
 
 	this.fluid = () => this.rMin + (((window.innerWidth / 100) - (this.minViewWidth / 100)) * (100 * (this.rMax - this.rMin) / (this.maxViewWidth - this.minViewWidth)));
 	this.r = clamp(this.fluid(), this.rMin, this.rMax);
 	this.x = !config.x ? this.r : config.x - this.r;
 	this.y = !config.y ? this.r : config.y - this.r;
 	this.rot = 0;
-	this.dx = 0.5;
-	this.dy = 0.5;
+	this.velocity = {x: 0.5, y: 0.5, rot: 0.005}
 	this.dRot = 0.005;
 }
 
@@ -46,19 +46,32 @@ Circle.prototype.draw = function() {
 Circle.prototype.move = function() {
 
 	if(this.x > (window.innerWidth - this.r) || this.x < this.r ) {
-		this.dx = -this.dx;
-		this.dRot = -this.dRot;
+		this.velocity.x = -this.velocity.x;
+		this.velocity.rot = -this.velocity.rot;
 	}
 	if(this.y > (window.innerHeight - this.r) || this.y < this.r ) {
-		this.dy = -this.dy;
-		this.dRot = -this.dRot;
+		this.velocity.y = -this.velocity.y;
+		this.velocity.rot = -this.velocity.rot;
 	}
 
-	this.x += this.dx;
-	this.y += this.dy;
-	this.rot += this.dRot;
+	this.x += this.velocity.x;
+	this.y += this.velocity.y;
+	this.rot += this.velocity.rot;
 
 	this.draw();
+}
+
+Circle.prototype.collide = function(circ) {
+	const dx = this.x - circ.x;
+	const dy = this.y - circ.y;
+
+	const distance = Math.hypot(dx, dy);
+	const sumRadius = this.r + circ.r; 
+
+	if(distance < sumRadius) {
+		resolveCollision(this, circ);
+		console.log("collided");
+	}
 }
 
 const canvas = new Canvas();
@@ -68,16 +81,74 @@ const circle2 = new Circle(canvas, {x: window.innerWidth, y: window.innerHeight 
 canvas.setSize();
 
 circle1.draw();
+
 circle2.draw();
 
 animate(function() {
 	canvas.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 	circle1.move();
+	circle1.collide(circle2);
+	
 	circle2.move();
+	circle2.collide(circle1);
 })
 
 onResize(function() {
 	canvas.setSize();
 	canvas.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-	circle1.move();
+
+	circle1.x = circle1.r;
+	circle1.y = circle1.r;
+
+	circle2.x = window.innerWidth - circle2.r;
+	circle2.y = window.innerHeight - circle2.r - 80;
 });
+
+// Collision Function -> https://gist.github.com/christopher4lis/f9ccb589ee8ecf751481f05a8e59b1dc
+
+function rotate(velocity, angle) {
+    const rotatedVelocities = {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    };
+
+    return rotatedVelocities;
+}
+
+function resolveCollision(particle, otherParticle) {
+    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+    const xDist = otherParticle.x - particle.x;
+    const yDist = otherParticle.y - particle.y;
+
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+        // Grab angle between the two colliding particles
+        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        // Store mass in var for better readability in collision equation
+        const m1 = particle.mass;
+        const m2 = otherParticle.mass;
+
+        // Velocity before equation
+        const u1 = rotate(particle.velocity, angle);
+        const u2 = rotate(otherParticle.velocity, angle);
+
+        // Velocity after 1d collision equation
+        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+        // Final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        particle.velocity.x = vFinal1.x;
+        particle.velocity.y = vFinal1.y;
+
+        otherParticle.velocity.x = vFinal2.x;
+        otherParticle.velocity.y = vFinal2.y;
+    }
+}
